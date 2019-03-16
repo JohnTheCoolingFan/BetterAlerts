ce4039f531d0b894aea65038759e44289dfd0cfb
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-03-16 16:28:04 +0300

    Added sprites prototypes

---
+++ data.lua
@@ -0,0 +1,74 @@
+data:extend({
+    {
+        type = "sprite",
+        name = "BA-entity-destroyed",
+        filename = "__core__/graphics/danger-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-entity-under-attack",
+        filename = "__core__/graphics/destroyed-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-not-enough-construction-robots",
+        filename = "__core__/graphics/not-enough-construction-robots-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-no-material-for-construction",
+        filename = "__core__/graphics/no-building-material-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-not-enough-repair-packs",
+        filename = "__core__/graphics/not-enough-repeir-packs-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-turret-fire",
+        filename = "__core__/graphics/warning-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-no-storage",
+        filename = "__core__/graphics/no-storage-space-icon.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-train-out-of-fuel",
+        filename = "__core__/graphics/fuel-icon-red.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    },
+    {
+        type = "sprite",
+        name = "BA-fluid-mixing",
+        filename = "__core__/graphics/fluid-icon-red.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64
+    }
+})
